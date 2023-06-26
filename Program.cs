4068be2261d2f9fbbcbbc81eb3f6daf1ba3e2941
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-06-26 17:35:08 -0500

    added notes on namespace and using System

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// using System; - indicates that our program will be using code that is 
+// in the .Net Namespace
+using System;
+
+
+// Namespace - Help organize programs, and prevent names that are in your
+// code from colliding with names that are in other third party libraries you might
+// include later
+namespace HelloWorld
+{
+    class Program{
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
